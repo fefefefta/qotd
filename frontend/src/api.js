8d6e4e7ff84ae7ef1cb2b9506785d652c00d6610
fefta <stuffefta@gmail.com>
2024-06-29 13:41:07 +0300

    fron deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', // Замените на URL вашего бэкенда
+  baseURL: 'https://qotd-api-5zh9.onrender.com/api/v1', // Замените на URL вашего бэкенда
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
